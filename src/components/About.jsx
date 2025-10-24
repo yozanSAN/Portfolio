796a2ea1d560e3a50fc400cbb7348baec13fa89a
yozanSAN <ayoubpro183@gmail.com>
2025-10-24 17:03:31 +0100

    Updated favicon and About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
         <div className="mt-10 text-center">
           <a 
             href="/resume.pdf" 
-            download="Your_Name_Resume.pdf"
+            download="Almobaraki-Ayoub-CV.pdf"
             className="inline-block px-6 py-3 bg-slate-800 dark:bg-slate-700 text-white rounded-lg hover:bg-slate-700 dark:hover:bg-slate-600 transition"
           >
             Download Resume
